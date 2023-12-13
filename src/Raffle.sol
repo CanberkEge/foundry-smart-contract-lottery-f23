de355ada239f98952a3b340cd1e320d8122b3182
Canberk"nnto <canberk2396u@hotmail.com"n>
2023-12-13 05:42:10 +0300

    Son commitler yapıldı

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,6 +24,8 @@
 
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * @title A sample Raffle contract
  * @author Canberk Ege Erden
@@ -34,29 +36,82 @@ pragma solidity ^0.8.18;
 contract Raffle {
     error Raffle__NotEnoughETHSent();
 
-    uint256 private immutable i_enteranceFee;
+    /** State Variables */
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    uint32 private immutable i_callbackGasLimit;
+
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     // what data structure should we use? How to keep track of all the participants?
 
-    constructor(uint256 enteranceFee) {
-        i_enteranceFee = enteranceFee;
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
         //  require(msg.value >= i_enteranceFee, "Not enough ETH sent!");
 
-        if (msg.value < i_enteranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHSent();
         }
         s_players.push(payable(msg.sender));
+        // 1. Makes migration easier
+        // 2. Makes front end "indexing" easier
+        emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    // 1. Get a random number
+    // 2. Use the random number to pick a player
+    // 3. Be automatically called
+
+    function pickWinner() public {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gas lane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS, // how many confirmations we want
+            i_callbackGasLimit, // max gas that we want the callback function to do
+            NUM_WORDS // number of more random numbers
+        );
+    }
+
+    // 1. Request the RNG
+    // 2. Get the random number
 
     /** Getter Function */
 
-    function getEnteranceFee() public view returns (uint256) {
-        return i_enteranceFee;
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
     }
 }
